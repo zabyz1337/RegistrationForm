bf7f9821e76228fab97cf2004062a235bf2b42b6
IKolmykov <zabyz1337@gmail.com>
2026-02-27 11:38:02 +0100

    render RegistrationForm in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import RegistrationForm from "./components/RegistrationForm";
+
 export default function App() {
   return (
     <div>
       <h1>Registration</h1>
+      <RegistrationForm />
     </div>
   );
 }
